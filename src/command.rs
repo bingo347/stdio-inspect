91c0116485009b14c8b37c9da9b93207c5a9309e
Dmitriy Belyaev <mail@d-belyaev.ru>
2020-11-27 00:58:08 +0300

    feat(command): realese Command with constructor

---
+++ src/command.rs
@@ -0,0 +1,44 @@
+use std::{
+    env::ArgsOs,
+    convert::{TryFrom, TryInto},
+    ffi::OsString
+};
+
+#[derive(Debug)]
+pub struct Command {
+    command: OsString,
+    args: ArgsOs
+}
+
+impl Command {
+    pub fn new<T: TryInto<Command, Error=()>>(input: T) -> Option<Self> {
+        match input.try_into() {
+            Ok(command) => Some(command),
+            _ => None
+        }
+    }
+}
+
+macro_rules! impl_try_from {
+    ($t:ty => |$input:pat| $body:block) => {
+        impl TryFrom<$t> for Command {
+            type Error = ();
+            fn try_from($input: $t) -> Result<Self, ()> {
+                $body
+            }
+        }
+    };
+}
+impl_try_from![ArgsOs => |mut args| {
+    match args.next() {
+        Some(command) => Ok(Self { command, args }),
+        _ => Err(())
+    }
+}];
+impl_try_from![(OsString, ArgsOs) => |(command, args)| {
+    Ok(Self { command, args })
+}];
+impl_try_from![(String, ArgsOs) => |(command, args)| {
+    let command = command.into();
+    Ok(Self { command, args })
+}];
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod config;
+mod command;
 
 fn main() {
     let config = config::collect_config();
