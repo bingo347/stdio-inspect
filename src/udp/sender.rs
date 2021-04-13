2714532852fbdc049ce84168a760e18a77f006d1
Dmitry Belyaev <mail@d-belyaev.ru>
2021-04-14 01:03:12 +0300

    feat(udp/sender): add send ticker

--- src/udp/sender.rs
+++ src/udp/sender.rs
@@ -1,29 +1,67 @@
 use crate::communicator::{Message, StreamKind, DATA_BUFFER_SIZE};
-use std::net::SocketAddr;
-use tokio::sync::broadcast::Receiver;
-use tokio::time::{self, Duration};
+use std::{net::SocketAddr, sync::Arc};
+use tokio::{
+    sync::{broadcast::Receiver, watch, Mutex},
+    time::{self, Duration, Instant},
+};
 
 const ACCUMULATOR_CAPACITY: usize = DATA_BUFFER_SIZE * 4;
+const TICK_DURATION: Duration = Duration::from_millis(500);
 
 pub async fn start_sender(addr: SocketAddr, mut rx: Receiver<Message>) {
-    let mut acc = Accumulator {
-        addr,
-        last_stream: None,
-        stream_data: Vec::with_capacity(ACCUMULATOR_CAPACITY),
-    };
-    tokio::spawn(async move {
-        loop {
-            tokio::select! {
-                _ = time::sleep(Duration::from_millis(500)) => {
-                    acc.tick().await;
-                },
-                msg = rx.recv() => {
-                    acc.push(msg.unwrap()).await;
-                },
+    let ticker = Ticker::new();
+    let mut tick_rx = Arc::clone(&ticker).run();
+    let acc = Arc::new(Mutex::new(Accumulator::new(addr)));
+    tokio::spawn({
+        let acc = Arc::clone(&acc);
+        async move {
+            while tick_rx.changed().await.is_ok() {
+                let mut acc = acc.lock().await;
+                acc.tick().await;
             }
         }
     });
+    tokio::spawn(async move {
+        loop {
+            let msg = rx.recv().await.unwrap();
+            ticker.reset().await;
+            let mut acc = acc.lock().await;
+            acc.push(msg).await;
+        }
+    });
     tokio::task::yield_now().await;
+}
+
+struct Ticker {
+    next_tick_time: Mutex<Instant>,
+}
+
+impl Ticker {
+    fn new() -> Arc<Self> {
+        Arc::new(Self {
+            next_tick_time: Mutex::new(Instant::now() + TICK_DURATION),
+        })
+    }
+
+    fn run(self: Arc<Self>) -> watch::Receiver<()> {
+        let (tx, rx) = watch::channel(());
+        tokio::spawn(async move {
+            loop {
+                time::sleep(Duration::from_millis(10)).await;
+                let mut next_tick_time = self.next_tick_time.lock().await;
+                if let Some(_) = next_tick_time.checked_duration_since(Instant::now()) {
+                    *next_tick_time = Instant::now() + TICK_DURATION;
+                    tx.send(()).unwrap();
+                }
+            }
+        });
+        rx
+    }
+
+    async fn reset(&self) {
+        let mut next_tick_time = self.next_tick_time.lock().await;
+        *next_tick_time = Instant::now() + TICK_DURATION;
+    }
 }
 
 struct Accumulator {
@@ -33,6 +71,14 @@ struct Accumulator {
 }
 
 impl Accumulator {
+    fn new(addr: SocketAddr) -> Self {
+        Self {
+            addr,
+            last_stream: None,
+            stream_data: Vec::with_capacity(ACCUMULATOR_CAPACITY),
+        }
+    }
+
     async fn tick(&mut self) {
         let kind = match self.last_stream {
             Some(k) => k,
